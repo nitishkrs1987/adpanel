58c293e307085845ff8a81aaf266bbbadc52466f
nitishkrs1987 <nitishkrs1987@gmail.com>
2018-07-12 16:54:24 +0530

    fixed live url generate issue

--- app/controllers/generate.js
+++ app/controllers/generate.js
@@ -135,7 +135,7 @@ function unicountry(campaign_id,camp_type,callback)
   database = new Database();
   gen_helper = new generate_helpers();
   var generate = "var a = Math.round(new Date().getTime() / 1000);";
-  database.query("select A.*,B.live_url from advertisor A,campaign B where A.campaign_id=B.id and A.active=1 and A.campaign_id="+campaign_id).then( rows => {
+  database.query("select * from advertisor where active=1 and campaign_id="+campaign_id).then( rows => {
     advertisor = rows;
     var ids= "";
     var multi_prod_ids= "";
